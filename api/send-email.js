991d064c519e7cfd16796b467473b3199ca44fa3
kiyantav <113287921+kiyantav@users.noreply.github.com>
2025-08-19 10:47:04 +0100

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -16,7 +16,7 @@ module.exports = async function handler(req, res) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        from: 'submissions.barbersmiths.co.uk', // Use a verified sender address
+        from: 'contact@submissions.barbersmiths.co.uk', // Use a verified sender address
         to: 'contact@barbersmiths.co.uk',
         subject: 'Daily Submission',
         html: '<p>Test submission</p>'
